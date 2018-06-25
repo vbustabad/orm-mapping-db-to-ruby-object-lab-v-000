ec44d440bedca853a0001444e33e81a75d431f40
Veronica Bustabad <vbustabad@gmail.com>
2018-06-25 13:22:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,9 +80,7 @@ class Student
     LIMIT 1
     SQL
 
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
 
   def self.all_students_in_grade_X(grade)
