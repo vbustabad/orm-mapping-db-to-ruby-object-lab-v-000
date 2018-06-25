a0e9106717f81469a21e325291f54b900812f043
Veronica Bustabad <vbustabad@gmail.com>
2018-06-25 12:25:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,6 +51,15 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.first_X_students_in_grade_10
+  end
+
+  def self.first_student_in_grade_10
+  end
+
+  def self.all_students_in_grade_X
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
