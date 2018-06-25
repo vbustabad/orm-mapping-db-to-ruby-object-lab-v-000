5ad4232c5d131b205f0c47e9d0ebb3d8bc4ed1f9
Veronica Bustabad <vbustabad@gmail.com>
2018-06-25 13:09:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
     WHERE grade = "10"
     SQL
 
-    DB[:conn].execute(sql, number).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
