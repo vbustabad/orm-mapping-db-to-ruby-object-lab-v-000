5649258b7be7c63502d987d6a29074622282369e
Veronica Bustabad <vbustabad@gmail.com>
2018-06-25 12:26:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,6 +55,14 @@ class Student
   end
 
   def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql)
   end
 
   def self.all_students_in_grade_X
